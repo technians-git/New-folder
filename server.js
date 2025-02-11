9f67c22f94ba985ff3ee2abda8f3023a1141732c
Mritunjay Paswan <mritunjay.paswan@technians.com>
2025-02-11 17:15:23 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+
+const app = express()
+
+
+app.listen(8081, ()=> console.log('Server is running on port 8081'))
+
+app.get('/', (req, res)=>{
+    res.json({
+        success:true,
+        message: "Hello from the server"
+    })
+})
